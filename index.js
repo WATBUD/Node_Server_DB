a870445b80fea4428b777dba97f8fa13c6c7aedc
louischin <z23320785@gmail.com>
2022-04-02 00:52:01 +0800

    add

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const verification_DB =require("./verification_DB");
-console.log('%c verification_DB','background: red; color: white',verification_DB.foo);
-
-
 const express = require("express");
 const app = express();
 const port = 3000;
+
+
+
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+
 app.get("/products", (req,res) => {
   const products = [
   {
@@ -22,10 +25,19 @@ app.get("/products", (req,res) => {
     name: "wrench",
   },
  ];
-
+ console.log(this);
+ console.log('%c this','background: red; color: white',this);
+ console.log('%c fooo','background: red; color: white',verification_DB.fooo);
+ console.log('%c sadadad','background: red; color: white',verification_DB.sadadad);
+ console.log('%c axd','background: red; color: white',verification_DB.axd);
  res.json(products);
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port, (
+) => {
+
+console.log(`Example app listening on port ${port}!`);
+});
 
 
 
--- verification_DB.js
+++ verification_DB.js
@@ -7,28 +7,66 @@ const verification_DB = mysql.createConnection({
   port: '3306'
 });
 var axd = 55;
-const foo = Math.sqrt(2);
-verification_DB.connect(function (err, rows, fields) {
-  if (err) throw err;
-  console.log("verification_DB Connected!");
-  // console.log('%c rows','background: red; color: white',rows);
-  // console.log('%c fields','background: red; color: white',fields);
-  verification_DB.query('SELECT * FROM `Customer` WHERE `ID`=1', function (err, rows, fields, results) {
-    console.log('err: ', err);
-    console.log('rows: ', rows);
+const fooo = Math.sqrt(2);
+console.log("verification_DB instance!");
 
-    //console.log('fields: ',fields);
-    //console.log('results: ',results);
-    verification_DB.end();
-    var process = require('process');
-    if (process) {
-      console.log(`This process is pid ${process.pid}`);
-      console.log('This process is pid ' + process.pid);
-      process.exit(0);
-    }
+
+
+
+var certification_Insert = "INSERT INTO `Certification`(`SN`, `Model Name`, `Customer ID`, `Model ID`, `MCU ID`, `Year`, `Month`, `Day`, `Black List`) VALUES ('1','2','3','4','5','6','7','8','9')";
+var certification_Select = "SELECT * FROM `Customer` WHERE `ID`=1";
+
+
+
+//var _setCommand=setCommand();
+//console.log('%c _setCommand','background: red; color: white',_setCommand);
+
+function setCommand(){
+  verification_DB.connect(function (err, rows, fields) {
+    if (err) throw err;
+    console.log("verification_DB Connected!");
+    // console.log('%c rows','background: red; color: white',rows);
+    // console.log('%c fields','background: red; color: white',fields);
+    verification_DB.query(certification_Insert, function (err, rows, fields, results) {
+      console.log('err: ', err);
+      console.log('rows: ', rows);
+      mysql.in
+      //console.log('fields: ',fields);
+      console.log('results: ', results);
+      verification_DB.end();
+      return rows;
+      var process = require('process');
+      if (process) {
+        console.log(`This process is pid ${process.pid}`);
+        console.log('This process is pid ' + process.pid);
+        process.exit(0);
+      }
+    });
   });
+}
+function onGeneratedRow(row, connection) {
+  if (row.constructor.name == 'RowDataPacket') {
+    console.log('First Name: ', row.FirstName);
+    console.log('Last Name: ', row.LastName);
+  }
+}
 
-});
+
+
+
+// swpanSN(){
+
+
+
+// }
+
+
+
+
+
+//export default sadadad=this;
+
+
 // conn.query('SELECT 12 + 34 AS result', function(err, rows, fields) {
 //     if (err) throw err;
 //     console.log('The result is: ', rows[0].result);
